1ac3c9dc7dd9b84824bf0378b69bde78c5ff722c
rochafialvin <rochafi.dev@gmail.com>
2022-01-13 11:33:46 +0700

    Add documentation

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ const port = 2022;
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  // Membuat informasi tambahan response untuk client
   const header = {
+    // Jenis data yang akan dikirim bertipe json
     "Content-Type": "application/json",
+    // Menentukan siapa saja yang boleh mengakses
     "Access-Control-Allow-Origin": "*",
+    // Menentukan methos apa saja yang dapat mengakses api
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
   };
 
-  const todosFile = fs.readFileSync("./data/todos.json");
-  const todos = JSON.parse(todosFile);
-  console.log(todos);
+  const todosBuffer = fs.readFileSync("./data/todos.json");
+  console.log({ todosBuffer });
+  const todos = JSON.parse(todosBuffer);
 
+  // Setup informasi tambahan untuk dikirimkan dalam response bersamaan dengan data yang hendak dikirim (todos)
   res.writeHead(200, header);
+  // Menadakan proses request berakhir dengan mengirim data (todos)
   res.end(JSON.stringify(todos));
 });
 
